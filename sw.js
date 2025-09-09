0db7c77cb72a2a9118ceeef365418e5fa3469dcb
tobiasinscore422008-maker <tobiasinscore422008@gmail.com>
2025-09-09 08:35:49 -0300

    Sw

---
+++ sw.js
@@ -0,0 +1,30 @@
+
+const CACHE_NAME = 'cuaderno-movil-v1';
+const ASSETS = [
+  './cuaderno_movil.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match('./cuaderno_movil.html')))
+  );
+});
